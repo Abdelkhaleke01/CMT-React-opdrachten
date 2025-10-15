e7af341eb4527601b546ef91284468c1da8a6d22
Abdelkhaleke01 <9024558@student.zadkine.nl>
2025-10-15 11:28:13 +0200

    1.3

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,9 +1,20 @@
+import React, { useState } from 'react';
+ 
 const Book = ({title, desc, img}) => {
+    const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
+
+    const verhoogTeller = () => {
+        setAantalKeerGelezen(aantal => aantal + 1);
+    };
+
     return (
         <section className="Books-container">
             <img src={img} alt={title} />
             <p>{desc}</p>
             <h1>{title}</h1>
+            <button onClick={verhoogTeller}>
+                Keer gelezen: {aantalKeerGelezen}
+            </button>
         </section>
      );
 }
